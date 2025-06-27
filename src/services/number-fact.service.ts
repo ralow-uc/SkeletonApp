2dc26c8710d7836dfe3ac0c814551a243795e7b6
Raul Low <raulou92@gmail.com>
2025-06-26 20:56:39 -0400

    feat: offline storage

--- src/services/number-fact.service.ts
+++ src/services/number-fact.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { catchError, map, of } from "rxjs";
 
-@Injectable({ providedIn: "root" })
+@Injectable({
+  providedIn: "root",
+})
 export class NumberFactService {
+  private baseUrl = "http://numbersapi.com";
+
   constructor(private http: HttpClient) {}
 
-  getFact(num: number): Observable<string> {
-    return this.http.get(`http://numbersapi.com/${num}`, {
-      responseType: "text",
-    });
+  getFact(n: number) {
+    const storedFact = localStorage.getItem(`fact-${n}`);
+    if (storedFact) {
+      return of(storedFact);
+    }
+
+    return this.http.get(`${this.baseUrl}/${n}`, { responseType: "text" }).pipe(
+      map((fact) => {
+        localStorage.setItem(`fact-${n}`, fact);
+        return fact;
+      }),
+      catchError((error) => {
+        console.error("Error al obtener dato desde API:", error);
+        return of(`Sin conexión. No hay dato guardado para el número ${n}.`);
+      })
+    );
   }
 }
